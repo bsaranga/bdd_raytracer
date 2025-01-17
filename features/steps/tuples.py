8c9e83297a74bbf32ed61fe3595ab9f444624373
Saranga B <bsaranga@gmail.com>
2025-01-17 22:58:31 +0530

    Add initial implementation for tuple feature and step definitions

---
+++ features/steps/tuples.py
@@ -0,0 +1,44 @@
+#pylint: disable=all
+"""Basic tutorial for behave"""
+
+from behave import *
+""" 
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False """
+    
+@given('a <- tuple (4.3, -4.2, 3.1, 1.0)')
+def step_impl(context):
+    context.a = (4.3, -4.2, 3.1, 1.0)
+    
+@then('a.x = 4.3')
+def step_impl(context):
+    assert context.a[0] == 4.3
+    
+@then('a.y = -4.2')
+def step_impl(context):
+    assert context.a[1] == -4.2
+    
+@then('a.z = 3.1')
+def step_impl(context):
+    assert context.a[2] == 3.1
+    
+@then('a.w = 1.0')
+def step_impl(context):
+    assert context.a[3] == 1.0
+    
+@then('a is a point')
+def step_impl(context):
+    assert context.a[3] == 1.0
+    
+@then('a is not a vector')
+def step_impl(context):
+    assert context.a[3] != 0.0
